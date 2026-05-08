c904d588b15bc6d195d3c4cb5ca17bdfba0aa496
DEV-COM <48807773+dev-com2020@users.noreply.github.com>
2026-05-08 15:33:26 +0200

    Format variables in variables.tf for consistency

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -1,6 +1,12 @@
 variable "project"       { type = string }
-variable "instance_type" { type = string; default = "t3.small" }
+variable "instance_type" {
+     type = string
+default = "t3.small"
+ }
 variable "subnet_id"     { type = string }
 variable "sg_id"         { type = string }
 variable "key_name"      { type = string }
-variable "tags"          { type = map(string); default = {} }
+variable "tags"          { 
+    type = map(string)
+ default = {} 
+ }
