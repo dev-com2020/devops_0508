9fef20de727fc35896fa8b1430fa0bb755dae5f2
DEV-COM <48807773+dev-com2020@users.noreply.github.com>
2026-05-08 15:57:44 +0200

    Update main.tf

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -4,12 +4,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
