69e38fb36987903a7b5aea93d3a18754fe505ce7
DEV-COM <48807773+dev-com2020@users.noreply.github.com>
2026-05-08 15:33:04 +0200

    Format variable definitions in variables.tf

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,7 +1,16 @@
 variable "project"          { type = string }
-variable "instance_type"    { type = string; default = "t3.small" }
+variable "instance_type"    {
+     type = string
+ default = "t3.small" 
+ }
 variable "subnet_id"        { type = string }
 variable "sg_id"            { type = string }
 variable "key_name"         { type = string }
-variable "root_volume_size" { type = number; default = 20 }
-variable "tags"             { type = map(string); default = {} }
+variable "root_volume_size" { 
+    type = number 
+    default = 20 
+    }
+variable "tags"             {
+     type = map(string)
+ default = {} 
+ }
